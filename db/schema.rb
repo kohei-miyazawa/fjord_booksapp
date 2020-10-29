72279b8168f8fcd94e16a9eb9ef68306e1fd83d7
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-10-30 00:50:42 +0900

    add author

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_145035) do
+ActiveRecord::Schema.define(version: 2020_10_29_154536) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
