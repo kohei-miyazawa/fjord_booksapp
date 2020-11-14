2d81dff400e3e063f3a5aedaa2071dcc068f171b
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-11-14 23:22:28 +0900

    日本語と英語の切替機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
